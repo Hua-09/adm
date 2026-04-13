71bf8719ea1187e33ff33374e24875934a670766
Zhanyan Hua <3021964226@qq.com>
2026-04-13 16:13:15 +0800

    Update atomic.go

--- adm_bkd/utils/storage/atomic.go
+++ adm_bkd/utils/storage/atomic.go
@@ -1,41 +1,19 @@
 package storage
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 )
 
-// atomicWrite writes data to path atomically using a temp-file + rename.
-func atomicWrite(path string, data []byte, perm os.FileMode) error {
+func WriteFileAtomic(path string, data []byte, perm os.FileMode) error {
 	dir := filepath.Dir(path)
 	if err := os.MkdirAll(dir, 0o755); err != nil {
-		return fmt.Errorf("mkdir %s: %w", dir, err)
+		return err
 	}
 
-	tmp, err := os.CreateTemp(dir, ".tmp-")
-	if err != nil {
-		return fmt.Errorf("create temp file: %w", err)
+	tmp := path + ".tmp"
+	if err := os.WriteFile(tmp, data, perm); err != nil {
+		return err
 	}
-	tmpName := tmp.Name()
-
-	if _, err := tmp.Write(data); err != nil {
-		tmp.Close()
-		os.Remove(tmpName)
-		return fmt.Errorf("write temp file: %w", err)
-	}
-	if err := tmp.Chmod(perm); err != nil {
-		tmp.Close()
-		os.Remove(tmpName)
-		return fmt.Errorf("chmod temp file: %w", err)
-	}
-	if err := tmp.Close(); err != nil {
-		os.Remove(tmpName)
-		return fmt.Errorf("close temp file: %w", err)
-	}
-	if err := os.Rename(tmpName, path); err != nil {
-		os.Remove(tmpName)
-		return fmt.Errorf("rename temp file: %w", err)
-	}
-	return nil
+	return os.Rename(tmp, path)
 }
