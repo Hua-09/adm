4ee1739674beee9d90bd30405017f33012d1c948
Zhanyan Hua <3021964226@qq.com>
2026-04-13 15:54:01 +0800

    Update err_code.go

--- adm_bkd/utils/err_mgr/err_code.go
+++ adm_bkd/utils/err_mgr/err_code.go
@@ -1,12 +1,29 @@
-package err_mgr
+package apiresult
 
-// Application-level error codes.
-const (
-	ErrOK            = 0
-	ErrInvalidParam  = 1001
-	ErrStorageRead   = 2001
-	ErrStorageWrite  = 2002
-	ErrStorageDelete = 2003
-	ErrAIForward     = 3001
-	ErrInternal      = 9999
+import (
+	errmgr "adm_bkd/utils/err_mgr"
 )
+
+type APIResult struct {
+	Success bool        `json:"success"`
+	Code    int         `json:"code"`
+	Msg     string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func NewAPIResult(code int, data interface{}) APIResult {
+	msg := errmgr.ErrStr(code)
+	ret := APIResult{false, code, msg, data}
+	if code == 0 {
+		ret.Success = true
+	}
+	return ret
+}
+
+func NewAPIResultWithStr(code int, msg string, data interface{}) APIResult {
+	ret := APIResult{false, code, msg, data}
+	if code == 0 {
+		ret.Success = true
+	}
+	return ret
+}
